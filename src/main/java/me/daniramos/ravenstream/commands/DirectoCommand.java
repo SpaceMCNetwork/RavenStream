0fa2364fc050a062b2a438e767ffe802322c5dc1
SpaceMC Network <126994541+SpaceMCNetwork@users.noreply.github.com>
2025-08-10 02:26:04 -0500

    Update DirectoCommand.java

--- src/main/java/me/daniramos/ravenstream/commands/DirectoCommand.java
+++ src/main/java/me/daniramos/ravenstream/commands/DirectoCommand.java
@@ -115,91 +115,51 @@ public class DirectoCommand implements SimpleCommand {
         return null;
     }
     
-    private static final int CHAT_WIDTH = 320;
-    private static final Map<Character, Integer> CHAR_WIDTHS = new HashMap<>();
-    
-    static {
-        CHAR_WIDTHS.put(' ', 4);
-        CHAR_WIDTHS.put('!', 2);
-        CHAR_WIDTHS.put('"', 5);
-        CHAR_WIDTHS.put('#', 6);
-        CHAR_WIDTHS.put('$', 6);
-        CHAR_WIDTHS.put('%', 6);
-        CHAR_WIDTHS.put('&', 6);
-        CHAR_WIDTHS.put('\'', 3);
-        CHAR_WIDTHS.put('(', 5);
-        CHAR_WIDTHS.put(')', 5);
-        CHAR_WIDTHS.put('*', 5);
-        CHAR_WIDTHS.put('+', 6);
-        CHAR_WIDTHS.put(',', 2);
-        CHAR_WIDTHS.put('-', 6);
-        CHAR_WIDTHS.put('.', 2);
-        CHAR_WIDTHS.put('/', 6);
-        CHAR_WIDTHS.put('0', 6);
-        CHAR_WIDTHS.put('1', 6);
-        CHAR_WIDTHS.put('2', 6);
-        CHAR_WIDTHS.put('3', 6);
-        CHAR_WIDTHS.put('4', 6);
-        CHAR_WIDTHS.put('5', 6);
-        CHAR_WIDTHS.put('6', 6);
-        CHAR_WIDTHS.put('7', 6);
-        CHAR_WIDTHS.put('8', 6);
-        CHAR_WIDTHS.put('9', 6);
-        CHAR_WIDTHS.put(':', 2);
-        CHAR_WIDTHS.put(';', 2);
-        CHAR_WIDTHS.put('<', 5);
-        CHAR_WIDTHS.put('=', 6);
-        CHAR_WIDTHS.put('>', 5);
-        CHAR_WIDTHS.put('?', 6);
-        CHAR_WIDTHS.put('@', 7);
-        CHAR_WIDTHS.put('A', 6);
-        CHAR_WIDTHS.put('B', 6);
-        CHAR_WIDTHS.put('C', 6);
-        CHAR_WIDTHS.put('D', 6);
-        CHAR_WIDTHS.put('E', 6);
-        CHAR_WIDTHS.put('F', 6);
-        CHAR_WIDTHS.put('G', 6);
-        CHAR_WIDTHS.put('H', 6);
-        CHAR_WIDTHS.put('I', 4);
-        CHAR_WIDTHS.put('J', 6);
-        CHAR_WIDTHS.put('K', 6);
-        CHAR_WIDTHS.put('L', 6);
-        CHAR_WIDTHS.put('M', 6);
-        CHAR_WIDTHS.put('N', 6);
-        CHAR_WIDTHS.put('O', 6);
-        CHAR_WIDTHS.put('P', 6);
-        CHAR_WIDTHS.put('Q', 6);
-        CHAR_WIDTHS.put('R', 6);
-        CHAR_WIDTHS.put('S', 6);
-        CHAR_WIDTHS.put('T', 6);
-        CHAR_WIDTHS.put('U', 6);
-        CHAR_WIDTHS.put('V', 6);
-        CHAR_WIDTHS.put('W', 6);
-        CHAR_WIDTHS.put('X', 6);
-        CHAR_WIDTHS.put('Y', 6);
-        CHAR_WIDTHS.put('Z', 6);
-        CHAR_WIDTHS.put('[', 4);
-        CHAR_WIDTHS.put('\\', 6);
-        CHAR_WIDTHS.put(']', 4);
-        CHAR_WIDTHS.put('^', 6);
-        CHAR_WIDTHS.put('_', 6);
-        CHAR_WIDTHS.put('`', 3);
-        CHAR_WIDTHS.put('a', 6);
-        CHAR_WIDTHS.put('b', 6);
-        CHAR_WIDTHS.put('c', 6);
-        CHAR_WIDTHS.put('d', 6);
-        CHAR_WIDTHS.put('e', 6);
-        CHAR_WIDTHS.put('f', 5);
-        CHAR_WIDTHS.put('g', 6);
-        CHAR_WIDTHS.put('h', 6);
-        CHAR_WIDTHS.put('i', 2);
-        CHAR_WIDTHS.put('j', 6);
-        CHAR_WIDTHS.put('k', 5);
-        CHAR_WIDTHS.put('l', 3);
-        CHAR_WIDTHS.put('m', 6);
-        CHAR_WIDTHS.put('n', 6);
-        CHAR_WIDTHS.put('o', 6);
-        CHAR_WIDTHS.put('p', 6);
-        CHAR_WIDTHS.put('q', 6);
-        CHAR_WIDTHS.put('r', 6);
-        CHAR_WIDTHS.put('s',
+    private String centerText(String text) {
+        int chatWidth = 320;
+        int textWidth = 0;
+        boolean isBold = false;
+        
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (c == '&' && i + 1 < text.length()) {
+                char code = text.charAt(i + 1);
+                if (code == 'l' || code == 'L') {
+                    isBold = true;
+                } else if (code == 'r' || code == 'R') {
+                    isBold = false;
+                }
+                i++;
+                continue;
+            }
+            // Ancho aproximado de los caracteres en Minecraft
+            int charWidth = 6; 
+            if ("filI:".indexOf(c) != -1) {
+                charWidth = 4;
+            } else if ("klmMWw".indexOf(c) != -1) {
+                charWidth = 7;
+            } else if (c == ' ') {
+                charWidth = 4;
+            }
+            
+            if (isBold) {
+                textWidth += charWidth + 1;
+            } else {
+                textWidth += charWidth;
+            }
+        }
+        
+        int spacesNeeded = (chatWidth - textWidth) / 8; // Ancho de un espacio
+        if (spacesNeeded <= 0) {
+            return text;
+        }
+
+        StringBuilder centeredText = new StringBuilder();
+        for (int i = 0; i < spacesNeeded / 2; i++) {
+            centeredText.append(" ");
+        }
+        centeredText.append(text);
+        
+        return centeredText.toString();
+    }
+}
